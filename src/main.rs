576632c88beef27768d1c082c42b2421ae302ddd
Heather Tweedie <rhtweedie12@gmail.com>
2024-09-01 20:27:26 +0100

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rustacean!");
+}
+
+fn solve(chars: Vec<char>, dictionary: Vec<String>) -> Vec<String> {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo() {
+        assert_eq!(
+            solve(
+                vec!['f', 'o', 'c'],
+                vec!["foo".to_string(), "bar".to_string()]
+            ),
+            vec!["foo"]
+        );
+    }
 }
