2e978ba0678cc096b87d7ef82d41d73460cdebc9
Heather Tweedie <rhtweedie12@gmail.com>
2024-09-04 20:52:47 +0100

    Check word contains centre character

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ fn main() {
     println!("Hello, Rustacean!");
 }
 
-fn solve(chars: &[char], dictionary: Vec<String>) -> Vec<String> {
+fn solve(chars: &[char], centre_char: char, dictionary: Vec<String>) -> Vec<String> {
     dictionary
         .into_iter()
-        .filter(|word| check_chars(chars, word))
+        .filter(|word| check_chars(chars, centre_char, word))
         .collect()
 }
 
 /// Returns whether all characters in `word` are contained in `chars`.
-fn check_chars(chars: &[char], word: &str) -> bool {
-    word.chars().all(|c| chars.contains(&c))
+fn check_chars(chars: &[char], centre_char: char, word: &str) -> bool {
+    word.chars().all(|c| chars.contains(&c)) && word.contains(centre_char)
 }
 
 #[cfg(test)]
@@ -23,6 +23,7 @@ mod tests {
         assert_eq!(
             solve(
                 &['f', 'o', 'c', 'e'],
+                'f',
                 vec!["foo".to_string(), "bar".to_string(), "coffee".to_string()]
             ),
             vec!["foo", "coffee"]
