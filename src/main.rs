097d36f72d99ad29269b0e5beb7ad5f8954126cb
Heather Tweedie <rhtweedie12@gmail.com>
2024-09-04 21:30:03 +0100

    Take command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::env::args;
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, Rustacean!");
+    // Take input of dictionary, character list and central character
+    let args: Vec<String> = args().collect();
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("  {} [dictionary file path] [character set]", args[0]);
+        return;
+    }
+    let chars: Vec<char> = args[2].chars().collect();
+    let dict_str = read_to_string(&args[1]).expect("Error reading dictionary.");
+    let dict: Vec<&str> = dict_str.lines().collect();
+    let results = solve(&chars, chars[0], &dict);
+    // Output results
+    for result in results {
+        println!("{}", result)
+    }
 }
 
-fn solve(chars: &[char], centre_char: char, dictionary: Vec<String>) -> Vec<String> {
+fn solve(chars: &[char], centre_char: char, dictionary: &[&str]) -> Vec<String> {
     dictionary
         .into_iter()
         .filter(|word| check_chars(chars, centre_char, word))
+        .map(|word| word.to_string())
         .collect()
 }
 
